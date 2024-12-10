3e2c1a08bd7d69d20bda4741474f3ac2c3c1266a
rafaella-git <135769724+rafaella-git@users.noreply.github.com>
2024-12-11 04:09:35 +1300

    file: separate build from run

--- remix_nz/cases/will/02-battery-overnight_2020-2035-2050/result/02-battery-overnight_2020-2035-2050.gsp
+++
@@ -1,9 +0,0 @@
-{
-    "name": "02-battery-overnight_2020-2035-2050",
-    "nodes": [
-    ],
-    "options": [
-    ],
-    "path": ".",
-    "workDir": "."
-}
--- remix_nz/process/run_scenario.py
+++ remix_nz/process/run_scenario.py
@@ -6,13 +6,15 @@
 
 
 # %%
-# Reading in model built previously
+# Import dependencies
+import os
+import time
 from remix.framework.api.instance import Instance
 from pathlib import Path
 
 
-group_name="dlr"
-indx=0
+
+
 
 # ### Define the years to run the optimisation and the demand file
 # The demand file and the years run determine the name of the case and its results
@@ -26,38 +28,28 @@ folder_dict = {
     "dlr": [dlr, [2020, 2030,2050]]
 }
 
-files_lst = scenario_dict[group_name][0]
-yrs_sel = scenario_dict[group_name][1] # [2020, 2025, 2030, 2035, 2040, 2045, 2050]
-yrs_str='-'.join([str(item) for item in yrs_sel])
-yrs_to_calc = [2020, 2025, 2030, 2035, 2040, 2045, 2050]
 
 
+group_name="dlr"
+case_name=f"h2-domestic_2020-2030-2050"
+scenario = "wind+"
 
-case_name=f"dlr_2020-2030-2050"
-data_dir = Path(f"../output/{scenario_dict[group_name][0]}/{case_name}/data")
 
 # Defining the directory the model data is written in (folder "data/" in the project directory)
+data_dir = Path(f"../project/{group_name}/{case_name}/data")
+results_dir = Path(f"../project/{group_name}/{case_name}/result")
 if not data_dir.exists():
     raise IOError("You need to build the model first!")
-m = {i: Instance(datadir=data_dir,index_names=False) for i in ["wind"]}
-m["Base"] = Instance(index_names=False,datadir=data_dir)
-
 
 # %% [markdown]
-# ### Executing optimization model in GAMS
-#
-# Here the model is executed and optimized using GAMS, using the function `run` from the Instance. 
-
-# %%
-# running GAMS from Python script
-scen = "wind+"
-
+# Executing optimization model in GAMS
 s1 = time.perf_counter()
+m = {i: Instance(datadir=data_dir,index_names=False) for i in ["wind"]}
+m["Base"] = Instance(index_names=False,datadir=data_dir)
 m["Base"].run(
     resultdir=results_dir,
-    #resultfile=f"{case_name}",
-    resultfile=f"{case_name}_{scen.replace('/', '_')}",
-    scendir=f"{scen}",
+    resultfile=f"{case_name}_{scenario.replace('/', '_')}",
+    scendir=f"{scenario}",
     solver="gurobi",
     threads=8,
     lo=4,
@@ -71,53 +63,26 @@ m["Base"].run(
     barorder=1
 )
 print(os. getcwd())
-
 e1 = time.perf_counter()
 d1=time.strftime("%Hh %Mm %Ss", time.gmtime(e1-s1))
-print(f"------------- Running {case_name} took {d1}.")
+print(f"------------- Running {case_name} with scenario {scenario} took {d1}.")
 
-
-
-
-# %% [markdown]
 # #### Explanation for command line arguments to GAMS function call
-#
 # `lo=3` : log option of GAMS; ensures that the output from GAMS will be visible in the terminal (`lo=4`
 #          will write out a log file)
-#
 # `modeldir` : path to model directory (can be set relative or absolute)
-#
 # `datadir` : instruction to look for `*.dat` files in a specific directory
-#
 # `scendir` : instruction to look for `*.dat` files in a specific directory; REMix default: datadir
-#
 # `resultdir` : instruction to store the resulting `*.gdx` file in a specific directory
-#
 # `resultfile` : allows to give a custom name to the output `*.gdx` file; REMix default: "remix"
-#
 # `postcalc` : instruction whether to do a post-calculation; REMix default: 1
-#
 # `names` : instruction to give out actual variable names in case of error messages; REMix default: 0
+# `roundts` : instruction to automatically round after-comma digits in large time series files where necessary to successfully read them in; REMix default: 0
+# `keep` : keeps temporary folder with `*.csv` files built during the model run, might be interesting for debugging purposes; REMix default: 0
 #
-# `roundts` : instruction to automatically round after-comma digits in large time series files where necessary to
-# successfully read them in; REMix default: 0
-#
-# `keep` : keeps temporary folder with `*.csv` files built during the model run, might be interesting for debugging
-# purposes; REMix default: 0
-#
-#
+
 # #### If the model is infeasible or a run crashes
-#
 # If you build a model on your own, it can always happen that it gets infeasible or the execution stops out of
 # some other reason. In that case, you can refer to the `run_remix.lst` file and look for the error marker `****`.
-#
-# You can open that file (it will be stored in the model_dir) in an editor and look for the error message.
-# Alternatively, you can also use the commandline tool grep to search for the pattern "**** Exec Error"  in the
-# file to see what is wrong.
-#
-#
-# ### Evaluation of results
-#
-# This is everything needed to run a REMix model. If you are interested in an example on how to evaluate and interpret
-# results, head over to part c of this tutorial. In part d, the bonus tasks, you will get an introduction to GAMS error
-# handling and model extensions.
+# You can open that file in an editor and look for the error message.
+# Alternatively, you can also use the commandline tool grep to search for the pattern "**** Exec Error"  in the file to see what is wrong.
